362b487662e188bfb9ca61cb6480798a476cb3b2
Cyril <garnettbzh@gmail.com>
2025-08-14 16:23:17 +0200

    Plusieurs méthodes de requêtage auto dans le Repo

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -15,8 +15,7 @@ final class SerieController extends AbstractController
     #[Route('/list/{page}', name: '_list', requirements: ['page' => '\d+'], defaults: ['page' => 1], methods: ['GET'])]
     public function list(SerieRepository $serieRepository, int $page, ParameterBagInterface $parameters): Response
     {
-       //$series = $serieRepository->findAll();
-
+        // Récupération de la configuration
         $nbPerPage = $parameters->get('serie')['nb_max'];
         $offset = ($page - 1) * $nbPerPage;
         $criterias = [
@@ -24,14 +23,15 @@ final class SerieController extends AbstractController
             'genre' => 'Drama',
         ];
 
+        // Récupération des séries avec pagination
         $series = $serieRepository->findBy(
             $criterias,
-            ['popularity' => 'DESC',],
+            ['popularity' => 'DESC'],
             $nbPerPage,
             $offset,
         );
 
-
+        // Calcul du nombre total de pages
         $total = $serieRepository->count($criterias);
         $totalPages = ceil($total / $nbPerPage);
 
@@ -42,18 +42,39 @@ final class SerieController extends AbstractController
         ]);
     }
 
+    #[Route('/liste-custom', name: '_custom_list')]
+    public function listCustom(SerieRepository $serieRepository): Response
+    {
+
+        // $series = $serieRepository->findSeriesByCustom(400.0, 8.0);
+        $series = $serieRepository->findSeriesWithDQL(400.0, 8.0);
+
+        // le requêtage SQL raw nécessite qu'on adapte le templace (firstAirDate -> first_air_date )
+        //$series = $serieRepository->findSeriesWithSQL(400.0, 8.0);
+
+
+        return $this->render('serie/list.html.twig',[
+            'series' => $series,
+            'page' => 1,
+            'total_pages' => 1,
+        ]);
+    }
+
     #[Route('/detail/{id}', name: '_detail')]
     public function detail(int $id, SerieRepository $serieRepository): Response
     {
+
         $serie = $serieRepository->find($id);
+
+
+        // $serie = $serieRepository->findSerieByIdCustom($id);
 
         if (!$serie) {
             throw $this->createNotFoundException('Pas de série pour cet id');
         }
 
         return $this->render('serie/detail.html.twig', [
-            'serie' => $serie
+            'serie' => $serie,
         ]);
     }
-
 }
--- src/Repository/SerieRepository.php
+++ src/Repository/SerieRepository.php
@@ -6,14 +6,64 @@ use App\Entity\Serie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Serie>
- */
+
 class SerieRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Serie::class);
+    }
+
+    public function findSeriesByCustom(float $popularity, float $vote): array
+    {
+
+        // utilisation du queryBuilder
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.popularity > :popularity OR s.firstAirDate > :date')
+            ->andWhere('s.vote < :vote')
+            ->orderBy('s.popularity', 'DESC')
+            ->addOrderBy('s.firstAirDate', 'DESC')
+            ->setParameter('popularity', $popularity)
+            ->setParameter('vote', $vote)
+            ->setParameter('date', new \DateTime('-5 years'))
+            ->setFirstResult(0)
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+    }
+
+    // utilisation du DQL sous forme d'une requette sous forme de chaine de caractères
+    public function findSeriesWithDQL(float $popularity, float $vote): array
+    {
+        // Requête DQL complète et correcte
+        $dql = "SELECT s FROM App\Entity\Serie s
+                WHERE (s.popularity > :popularity OR s.firstAirDate > :date) 
+                AND s.vote > :vote
+                ORDER BY s.popularity DESC, s.firstAirDate DESC";
+
+        return $this->getEntityManager()->createQuery($dql)
+            ->setMaxResults(10)
+            ->setFirstResult(0)
+            ->setParameter('popularity', $popularity)
+            ->setParameter('vote', $vote)
+            ->setParameter('date', new \DateTime('-5 years'))
+            ->getResult(); // Pas execute() !
+    }
+
+    // utilisation du SQL sous forme d'une requette sous forme de chaine de caractères
+    public function findSeriesWithSQL(float $popularity, float $vote): array
+    {
+        $sql = <<<SQL
+            SELECT * FROM serie s 
+            WHERE (s.popularity > :popularity OR s.first_air_date > :date)
+            AND s.vote > :vote
+            ORDER BY s.popularity DESC, s.first_air_date DESC
+            LIMIT 10 OFFSET 0
+        SQL;
+
+    $conn = $this->getEntityManager()->getConnection();
+    return  $conn->prepare($sql)->executeQuery(['popularity' => $popularity, 'date'=> (new \DateTime('-5 years'))->format('Y-m-d'), 'vote' => $vote])
+        ->fetchAllAssociative();
     }
 
     //    /**
