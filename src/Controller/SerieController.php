2be4cbecaea6975d8337a46e99cf7bbd7651aae6
Cyril <garnettbzh@gmail.com>
2025-08-18 13:45:36 +0200

    lien avec ressources datas

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -15,66 +15,63 @@ final class SerieController extends AbstractController
     #[Route('/list/{page}', name: '_list', requirements: ['page' => '\d+'], defaults: ['page' => 1], methods: ['GET'])]
     public function list(SerieRepository $serieRepository, int $page, ParameterBagInterface $parameters): Response
     {
-        // Récupération de la configuration
+        //$series = $serieRepository->findAll();
+
         $nbPerPage = $parameters->get('serie')['nb_max'];
         $offset = ($page - 1) * $nbPerPage;
         $criterias = [
-            'status' => 'Returning',
-            'genre' => 'Drama',
+//            'status' => 'Returning',
+//            'genre' => 'Drama',
         ];
 
-        // Récupération des séries avec pagination
         $series = $serieRepository->findBy(
             $criterias,
             ['popularity' => 'DESC'],
             $nbPerPage,
-            $offset,
+            $offset
         );
 
-        // Calcul du nombre total de pages
         $total = $serieRepository->count($criterias);
         $totalPages = ceil($total / $nbPerPage);
 
         return $this->render('serie/list.html.twig', [
-            'series' => $series,
-            'page' => $page,
-            'total_pages' => $totalPages,
-        ]);
+                'series' => $series,
+                'page' => $page,
+                'total_pages' => $totalPages,
+            ]
+        );
     }
 
     #[Route('/liste-custom', name: '_custom_list')]
     public function listCustom(SerieRepository $serieRepository): Response
     {
+        //$series = $serieRepository->findSeriesCustom(400, 8);
+        $series = $serieRepository->findSeriesWithDQL(400, 8);
 
-        // $series = $serieRepository->findSeriesByCustom(400.0, 8.0);
-        $series = $serieRepository->findSeriesWithDQL(400.0, 8.0);
+        // Le requêtage SQL raw nécessite qu'on adapte le template (firstAirDate -> first_air_date)
+        //$series = $serieRepository->findSeriesWithSQL(400, 8);
 
-        // le requêtage SQL raw nécessite qu'on adapte le templace (firstAirDate -> first_air_date )
-        //$series = $serieRepository->findSeriesWithSQL(400.0, 8.0);
-
-
-        return $this->render('serie/list.html.twig',[
+        return $this->render('serie/list.html.twig', [
             'series' => $series,
             'page' => 1,
-            'total_pages' => 1,
+            'total_pages' => 10,
         ]);
     }
+
+
 
     #[Route('/detail/{id}', name: '_detail')]
     public function detail(int $id, SerieRepository $serieRepository): Response
     {
-
         $serie = $serieRepository->find($id);
-
-
-        // $serie = $serieRepository->findSerieByIdCustom($id);
 
         if (!$serie) {
             throw $this->createNotFoundException('Pas de série pour cet id');
         }
 
         return $this->render('serie/detail.html.twig', [
-            'serie' => $serie,
+            'serie' => $serie
         ]);
     }
+
 }
